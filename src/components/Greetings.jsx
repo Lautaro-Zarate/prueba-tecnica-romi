db1106e0e84b4e6cb9168b8cad57ae31c9d54528
Lautaro Zarate <zlautaro43@gmail.com>
2025-08-28 18:35:27 -0300

    update: all views done

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,6 +1,7 @@
 export const Greetings = () => {
     return(
         <section className="greetings-container">
+            {/* CAMBIAR IMAGENES DE FONDO */}
             <h1>¡Bienvenidos a Romi!</h1>
             <p>Formulario de prueba para sintomas básicos</p>
         </section>
