4ad3ece755eef15f16f2fbd4ed1033dc8eb44504
Lautaro Zarate <zlautaro43@gmail.com>
2025-09-02 19:53:26 -0300

    update: added function to show pain level

--- src/components/Fieldsets.jsx
+++ src/components/Fieldsets.jsx
@@ -1,26 +1,36 @@
+import { useState } from "react"
+
 export const Fieldsets = ({label, title}) => {
+    const [showPainLevel, setShowPainLevel] = useState(false);
+    
+    const handleRadioChange = (event) => {
+        setShowPainLevel(event.target.value === 'Sí')
+    }
+
     return(
         <div>
             <fieldset>
                 <legend>{title}</legend>
                     <div className="radio-container">
                         <label>
-                            <input type="radio" name={label} value="Sí"/>
+                            <input type="radio" name={label} value="Sí" onChange={handleRadioChange}/>
                             Sí
                         </label>
                         <label>
-                            <input type="radio" name={label} value="No"/>
+                            <input type="radio" name={label} value="No" onChange={handleRadioChange}/>
                             No
                         </label>
                     </div>
-                    <div className="pain-level-container">
-                        <label>Nivel de dolor <span className="level">(1 al 10)</span></label>
-                        <input 
-                        type="number" 
-                        min={1}
-                        max={10}
-                        />
-                    </div>
+                    {showPainLevel && (
+                        <div className="pain-level-container">
+                            <label>Nivel de dolor <span className="level">(1 al 10)</span></label>
+                            <input 
+                            type="number" 
+                            min={1}
+                            max={10}
+                            />
+                        </div>
+                    )}
             </fieldset>
         </div>
     )
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,7 +1,6 @@
 export const Greetings = () => {
     return(
         <section className="greetings-container">
-            {/* CAMBIAR IMAGENES DE FONDO */}
             <h1>¡Bienvenidos a Romi!</h1>
             <p>Formulario de prueba para sintomas básicos</p>
         </section>
